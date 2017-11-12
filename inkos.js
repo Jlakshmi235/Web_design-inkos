5284d48ba1037c336babb5721a3ffde9127a336d
Jlakshmi235 <jlakshmi235@gmail.com>
2017-11-12 08:48:55 -0600

    prototype 2

--- inkos.js
+++ inkos.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-/* Function for Landing Page */    
+/* Common Functions */
+	$('.alert').hide();
+
+/* Functions for Index Page */    
 	var clickEvent = false;
 	$('#myCarousel').carousel({
 		interval:   4000	
@@ -19,10 +22,9 @@ $(document).ready(function(){
 		}
 		clickEvent = false;
 	});
-
-/* Function for Request Help Page */
-	$('.alert').hide();
-	$('#submitButton').on('click', function() {
+	
+/* Functions for Request Help Page */
+	$('#requestSubmitButton').on('click', function() {
 		var name = $('#nameInput').val().trim();
 		var street = $('#streetInput').val().trim();
 		var city = $('#cityInput').val().trim();
@@ -34,7 +36,7 @@ $(document).ready(function(){
 		var totime = $('#toTimeInput').val().trim();
 		$('.requiredInput').removeAttr("style");
 		if (name != "" && street != "" && city != "" && zip != "" && phone != "" && email != "" && date != "" && fromtime != "" && totime != ""){
-			$('#informationForm').trigger("reset");
+			$('#requestUserEntry').trigger("reset");
 			$('#emptyFieldAlert').hide();
 			$('#submitSuccessAlert').show();
 		}
@@ -71,112 +73,152 @@ $(document).ready(function(){
 	});
 
 /* Functions for Volunteer Help Page */
-	$("#emptySearchAlert").hide();
- 	var names = ['ABC Corporation', 'Miles Guerrera', 'XYZ Corp. Social Outreach', 'RoofsForAll (NGO)', 'Saviour Hurricane Relief Trust', 'XYZ Corp.', 'Red Cross of America', 'XYZ Corp. Social Outreach', 'Martha Lewis Charity', 'Chicago Storm Relief Group', 'JKL Corp. Social Outreach', 'Red Cross of America', 'Martha Lewis Charity', 'Carson Building Administration'];
- 	var address = ['2000 Sheridan Road', '1717 Hinman Ave.', '2134 Clark Ave.', '1254 Madison St.', '245 Sierra Ave.', '1002 Main St.', '2030 University Pl.', '2004 Pelican Ave.', '1243 Lark Rd.', '2134 Harriot St.', '245 Carlson Bld.', '123 Baker Ave.', '809 Morrison Bld.', '2113 Clark St.'];
- 	var miles = ['0.2 miles', '0.3 miles', '0.3 miles', '0.3 miles', '0.3 miles', '0.5 miles', '0.6 miles', '1.2 miles','1.7 miles', '3.0 miles', '3.7 miles', '4.0 miles', '4.3 miles', '5.0 miles', '5.0 miles']
- 	var email = ['outreach@abc.com', 'mguerrera@gmail.com', 'outreach@xyz.com', 'contactus@roofsforall.org', 'shrf@gmail.com', 'outreach@xyz.com', 'contact@redcross.org', 'outreach@xyz.com', 'marthacharity@gmail.com', 'stormrelief@gmail.com', 'outreach@jkl.com', 'contact@redcross.com', 'marthacharity@gmail.com', 'carsonbuilding@gmail.com'];
- 	var mobile = '222-777-9999';
- 	var dates = ['10/22/2017', '10/22/2017', '10/22/2017', '10/22/2017', '10/22/2017', '10/23/2017', '10/23/2017', '10/23/2017', '10/24/2017', '10/24/2017', '10/25/2017', '10/25/2017', '10/25/2017', '10/25/2017'];
- 	var times = ['3:00PM - 7:00PM', '2:00PM - 3:00PM', '4:00PM - 6:00PM', '2:00PM - 3:00PM', '4:00PM - 6:00PM', '12:00PM - 3:00PM', '6:00PM - 9:00PM', '5:00PM - 10:00PM', '11:00AM - 3:00PM', '4:00PM - 7:00PM', '6:00PM - 9:00PM', '11:00AM - 4:00PM', '12:00PM - 2:00PM', '4:00PM - 6:00PM'];
- 	var timefrom = ['3:00PM', '2:00PM', '4:00PM', '2:00PM', '4:00PM', '12:00PM', '6:00PM', '5:00PM', '11:00AM', '4:00PM', '6:00PM', '11:00AM', '12:00PM', '4:00PM'];
- 	var timeto = ['7:00PM', '3:00PM', '6:00PM', '3:00PM', '6:00PM', '3:00PM', '9:00PM', '10:00PM', '3:00PM', '7:00PM', '9:00PM', '4:00PM', '2:00PM', '6:00PM'];
- 	var miles = ['0.2 Miles', '0.3 Miles', '0.3 Miles', '0.3 Miles', '0.5 Miles', '0.6 Miles', '1.2 Miles', '1.7 Miles', '3.0 Miles', '3.7 Miles', '4.0 Miles', '4.3 Miles', '5.0 Miles', '5.0 Miles']
- 	var descs = ['ABC organization is distributing 1000 disaster relief kits for people affected. Volunteers needed', 'Lend your support to families affected in ...', 'XYZ Corp. is giving out free meals to individuals in need.', 'Volunteer your house as shelter for individuals/families who have lost their homes.', 'Lend your support (financial,emotional) to families and individuals affected in storm.', 'XYZ corporation is distributing 1500 disaster survival kits for people affected. Volunteers needed', 'Lend your support to families affected in ...', 'XYZ corp. is giving out free meals to families and individuals in need. Donations welcome.', 'Volunteer your house as shelter for individuals/families who had their houses destroyed.', 'Lend your support (financial, emotional, shelter) to families and individuals affected in storm.', 'JKL Corp. has pledged the distribution of 500 free meals. Volunteers required.', 'Red Cross is putting together school kits to be distributed amongst children below 14 years.', 'Lend your support to families affected in ....', 'Volunteer to shelter individuals/families who have lost their homes.'];
+ 	var volunteerNameArray = ['ABC Corporation', 'Miles Guerrera', 'XYZ Corp. Social Outreach', 'RoofsForAll (NGO)', 'Saviour Hurricane Relief Trust', 'XYZ Corp.', 'Red Cross of America', 'XYZ Corp. Social Outreach', 'Martha Lewis Charity', 'Chicago Storm Relief Group', 'JKL Corp. Social Outreach', 'Red Cross of America', 'Martha Lewis Charity', 'Carson Building Administration'];
+ 	var volunteerAddressArray = ['2000 Sheridan Road', '1717 Hinman Ave.', '2134 Clark Ave.', '1254 Madison St.', '245 Sierra Ave.', '1002 Main St.', '2030 University Pl.', '2004 Pelican Ave.', '1243 Lark Rd.', '2134 Harriot St.', '245 Carlson Bld.', '123 Baker Ave.', '809 Morrison Bld.', '2113 Clark St.'];
+ 	var volunteerDistanceArray = ['0.2 miles', '0.3 miles', '0.3 miles', '0.3 miles', '0.3 miles', '0.5 miles', '0.6 miles', '1.2 miles','1.7 miles', '3.0 miles', '3.7 miles', '4.0 miles', '4.3 miles', '5.0 miles', '5.0 miles']
+ 	var volunteerEmailArray = ['outreach@abc.com', 'mguerrera@gmail.com', 'outreach@xyz.com', 'contactus@roofsforall.org', 'shrf@gmail.com', 'outreach@xyz.com', 'contact@redcross.org', 'outreach@xyz.com', 'marthacharity@gmail.com', 'stormrelief@gmail.com', 'outreach@jkl.com', 'contact@redcross.com', 'marthacharity@gmail.com', 'carsonbuilding@gmail.com'];
+ 	var volunteerPhoneArray = '222-777-9999';
+ 	var volunteerDateArray = ['10/22/2017', '10/22/2017', '10/22/2017', '10/22/2017', '10/22/2017', '10/23/2017', '10/23/2017', '10/23/2017', '10/24/2017', '10/24/2017', '10/25/2017', '10/25/2017', '10/25/2017', '10/25/2017'];
+ 	var volunteerTimeArray = ['3:00PM - 7:00PM', '2:00PM - 3:00PM', '4:00PM - 6:00PM', '2:00PM - 3:00PM', '4:00PM - 6:00PM', '12:00PM - 3:00PM', '6:00PM - 9:00PM', '5:00PM - 10:00PM', '11:00AM - 3:00PM', '4:00PM - 7:00PM', '6:00PM - 9:00PM', '11:00AM - 4:00PM', '12:00PM - 2:00PM', '4:00PM - 6:00PM'];
+ 	var volunteerDescriptionArray = ['ABC organization is distributing 1000 disaster relief kits for people affected. Volunteers needed', 'Lend your support to families affected in ...', 'XYZ Corp. is giving out free meals to individuals in need.', 'Volunteer your house as shelter for individuals/families who have lost their homes.', 'Lend your support (financial,emotional) to families and individuals affected in storm.', 'XYZ corporation is distributing 1500 disaster survival kits for people affected. Volunteers needed', 'Lend your support to families affected in ...', 'XYZ corp. is giving out free meals to families and individuals in need. Donations welcome.', 'Volunteer your house as shelter for individuals/families who had their houses destroyed.', 'Lend your support (financial, emotional, shelter) to families and individuals affected in storm.', 'JKL Corp. has pledged the distribution of 500 free meals. Volunteers required.', 'Red Cross is putting together school kits to be distributed amongst children below 14 years.', 'Lend your support to families affected in ....', 'Volunteer to shelter individuals/families who have lost their homes.'];
 
-	$('#vol-table').on('click-row.bs.table', function (e, row, $element) {
-		/*Setting background color for table row */
-  	$('.success').removeClass('success');
-  	$($element).addClass('success');
-});
+	$('#volTable').on('click-row.bs.table', function (e, row, $element) {
+	/*Setting background color for table row */
+  		$('.success').removeClass('success');
+  		$($element).addClass('success');
+  	});
 
-$("#search-button").click(function(){
-/*Moving search bar/ checking for input*/
-    var input = $('#searchbar').val().trim();
-    if(input == ""){
-      $('#emptySearchAlert').show();
-      $('#searchbar').css("background-color", "LemonChiffon");
-      return false;
-    } 
-    else{
-    $('#searchbar').removeAttr("style");
-    $('#emptySearchAlert').hide();
-    $("#pad-div").slideUp(1000);
-    $("#table-div").show(1000);
-    return false;
-  }
+	$("#volunteerSearchButton").click(function(){
+	/*Moving search bar/ checking for input*/
+    	var input = $('#volunteerSearchBar').val().trim();
+    	if(input == ""){
+      		$('#emptySearchAlert').show();
+      		$('#volunteerSearchBar').css("background-color", "LemonChiffon");
+      		return false;
+    	} 
+    	else{
+    		$('#volunteerSearchBar').removeAttr("style");
+   			$('#emptySearchAlert').hide();
+    		$("#volTableDiv").show(1000);
+    		return false;
+  		}
     });
 
-
-$("#vol-table tbody > tr").click(function() {
-/*Setting request info fields and showing divs on modal open (table row click)*/
-    $("body").addClass("stop-scrolling");
-    $("#modalHead").html("Your help is needed "+miles[$(this).data('id')]+" away!");
-    current_row = $(this)
-    $("#name").html(names[$(this).data('id')]);      
-    $("#description").html(descs[$(this).data('id')]);
-    $("#times").html(times[$(this).data('id')]);
-    $("#date").html(dates[$(this).data('id')]);
-    $("#email").html(email[$(this).data('id')]);
-    $("#mobile").html(mobile);
-    $("#address").html(address[$(this).data('id')]);
-    var selected = $(this).hasClass("highlight");
-    $("#vol-table tr").removeClass("highlight");
-    if(!selected)
+	$("#volTable tbody > tr").click(function() {
+	/*Setting request info fields and showing divs on modal open (table row click)*/
+    	$("body").addClass("stop-scrolling");
+    	current_row = $(this);
+    	$("#nameOutput").html(volunteerNameArray[$(this).data('id')]);      
+    	$("#descriptionOutput").html(volunteerDescriptionArray[$(this).data('id')]);
+    	$("#timeOutput").html(volunteerTimeArray[$(this).data('id')]);
+    	$("#dateOutput").html(volunteerDateArray[$(this).data('id')]);
+    	$("#emailOutput").html(volunteerEmailArray[$(this).data('id')]);
+    	$("#phoneOutput").html(volunteerPhoneArray);
+    	$("#addressOutput").html(volunteerAddressArray[$(this).data('id')]);
+    	var selected = $(this).hasClass("highlight");
+    	$("#volTable tr").removeClass("highlight");
+    	if(!selected){
             $(this).addClass("highlight");
+    	}
+	});
 
-});
+	$("#form-modal").on('hidden.bs.modal', function(e){
+	/* Hiding divs and resetting forms in modal close */
+  		$("body").removeClass("stop-scrolling");
+  		current_row.removeClass("highlight");
+  		$("#volunteerInfoForm").slideDown()
+  		$("#volunteerUserEntry").addClass("hide")
+  		$('#emptyFieldAlert').hide();
+  		$('#submitSuccessAlert').hide();
+  		$('#volunteerUserEntry').trigger("reset");
+  		$("#volunteerModalHeader").text("Requester Information");
+	});
 
+	$(document).on("click", "#volunteerVolunteerButton", function(event){
+	/*Function to show volunteer info form in modal */
+    	$("#volunteerInfoForm").slideUp();
+    	$("#volunteerUserEntry").removeClass("hide");
+    	$("#volunteerModalHeader").text("Volunteer Information");
+	});
 
-// $("#form-modal").on('shown.bs.modal', function(e){
-//       $("body").addClass(".stop-scrolling");
-// });
+	$(document).on("click", "#volunteerSubmitButton", function(event){
+	/* Function for Volunteer info fields check*/
+    	var name = $('#nameInput').val().trim();
+    	var phone = $('#phoneInput').val().trim();
+    	var email = $('#emailInput').val().trim();
+    	$('.requiredInput').removeAttr("style");
+    	if (name != "" && phone != "" && email != ""){
+      		$('#emptyFieldAlert').hide();
+      		$('#submitSuccessAlert').show();
+      		$('#volunteerUserEntry').trigger("reset");
+    	}
+    	else{
+      		$('#emptyFieldAlert').show();
+      		if (name == ""){
+        		$('#nameInput').css("background-color", "LemonChiffon");
+      		}
+      		if (phone == ""){
+        		$('#phoneInput').css("background-color", "LemonChiffon");
+      		}
+      		if (email == ""){
+        		$('#emailInput').css("background-color", "LemonChiffon");
+      		}
+    	}
+  	});
 
-$("#form-modal").on('hidden.bs.modal', function(e){
-/* Hiding divs and resetting forms in modal close */
-  $("body").removeClass("stop-scrolling");
-  current_row.removeClass("highlight");
-  $("#info-form").slideDown()
-  $("#user-entry").addClass("hide")
-  $('#emptyFieldAlert').hide();
-  $('#submitSuccessAlert').hide();
-  $('#user-entry').trigger("reset");
-  $("#modal-header-title").text("Requester Information");
-});
+  	/* Functions for Find Shelter Page */
+  var shelterImageArray = ['images/library.jpg', 'images/tech.jpg', 'images/ryancenter.jpg', 'images/ryanfield.jpg', 'images/highschool.jpg'];
+ 	var shelterNameArray = ['Evanston Public Library', 'Northwestern Technological Institute', 'Ryan Center for the Musical Arts', 'Ryan Field', 'Evanston Township High School'];
+ 	var shelterAddressArray = ['1703 Orrington Ave.', '2145 Sheridan Road', '70 Arts Circle Dr.', '1505 Central St.', '1600 Dodge Ave.'];
+ 	var shelterPhoneArray = ['847-448-8600', '847-491-5220', '847-491-7575', '847-491-7887', '847-491-7887'];
+ 	var shelterTypeArray = ['Public Building', 'Private Building', 'Private Building', 'Private Building', 'Public Building'];
+ 	var shelterCapacityArray = ['500', '3000', '1500', '10000']
 
-$(document).on("click", "#volunteer-button", function(event){
-/*Function to show volunteer info form in modal */
-    $("#info-form").slideUp();
-    $("#user-entry").removeClass("hide");
-    $("#modal-header-title").text("Volunteer Information");
-});
+	$('#shelterTable').on('click-row.bs.table', function (e, row, $element) {
+	/*Setting background color for table row */
+  		$('.success').removeClass('success');
+  		$($element).addClass('success');
+  	});
 
+	$("#shelterSearchButton").click(function(){
+	/*Moving search bar/ checking for input*/
+    	var input = $('#shelterSearchBar').val().trim();
+    	if(input == ""){
+        $('#emptySearchAlert').show();
+      	$('#shelterSearchBar').css("background-color", "LemonChiffon");
+      	return false;
+    	} 
+    	else{
+    		$('#shelterSearchBar').removeAttr("style");
+   			$('#emptySearchAlert').hide();
+    		$("#shelterTableDiv").show(1000);
+    		return false;
+  		}
+    });
 
+	$("#shelterTable tbody > tr").click(function() {
+	/*Setting request info fields and showing divs on modal open (table row click)*/
+    	$("body").addClass("stop-scrolling");
+    	current_row = $(this);
+      $("#shelterImage").attr("src", shelterImageArray[$(this).data('id')]);
+    	$("#nameOutput").html(shelterNameArray[$(this).data('id')]);      
+    	$("#addressOutput").html(shelterAddressArray[$(this).data('id')]);
+    	$("#phoneOutput").html(shelterPhoneArray[$(this).data('id')]);
+    	$("#typeOutput").html(shelterTypeArray[$(this).data('id')]);
+    	$("#capacityOutput").html(shelterCapacityArray[$(this).data('id')]);
+    	var selected = $(this).hasClass("highlight");
+    	$("#shelterTable tr").removeClass("highlight");
+    	if(!selected){
+            $(this).addClass("highlight");
+    	}
+	});
 
-$(document).on("click", "#submitVolButton", function(event){
-/* Function for Volunteer info fields check*/
-    var name = $('#nameInput').val().trim();
-    var phone = $('#phoneInput').val().trim();
-    var email = $('#emailInput').val().trim();
-    $('.requiredInput').removeAttr("style");
-    if (name != "" && phone != "" && email != ""){
-      $('#emptyFieldAlert').hide();
-      $('#submitSuccessAlert').show();
-      $('#user-entry').trigger("reset");
-    }
-    else{
-      $('#emptyFieldAlert').show();
-      if (name == ""){
-        $('#nameInput').css("background-color", "LemonChiffon");
-      }
-      if (phone == ""){
-        $('#phoneInput').css("background-color", "LemonChiffon");
-      }
-      if (email == ""){
-        $('#emailInput').css("background-color", "LemonChiffon");
-      }
-    }
-  });
+	$("#form-modal").on('hidden.bs.modal', function(e){
+	/* Hiding divs and resetting forms in modal close */
+  		$("body").removeClass("stop-scrolling");
+  		current_row.removeClass("highlight");
+  		$("#shelterInfoForm").slideDown()
+	});
 });
 
 
